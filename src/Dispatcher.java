9e6a91406b3bb90b35a4061c7c86f037f4b4a9cd
권경민 <kkm0919@onycom.com>
2023-03-31 11:12:13 +0900

    Dispatcher에서 demultiplex 메소드 클래스로 분리

--- src/Dispatcher.java
+++ src/Dispatcher.java
@@ -15,35 +15,9 @@ public class Dispatcher {
         try {
             Socket socket = serverSocket.accept();
 
-            demultiplex(socket, handleMap);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void demultiplex(Socket socket, HandleMap handleMap) {
-        try {
-            InputStream inputStream = socket.getInputStream();
-
-            byte[] buffer = new byte[HEADER_SIZE];
-            inputStream.read(buffer);
-            String header = new String(buffer);
-
-            handleMap.get(header).handleEvent(inputStream);
-
-            socket.close();
-
-//            switch (header) {
-//                case "0x5001":
-//                    StreamSayHelloProtocol streamSayHelloProtocol = new StreamSayHelloProtocol();
-//                    streamSayHelloProtocol.handleEvent(inputStream);
-//                    break;
-//                case "0x6001":
-//                    StreamUpdateProfileProtocol streamUpdateProfileProtocol = new StreamUpdateProfileProtocol();
-//                    streamUpdateProfileProtocol.handleEvent(inputStream);
-//                    break;
-//            }
-
+            Runnable demultiplexer = new Demultiplexer(socket, handleMap);
+            Thread thread = new Thread(demultiplexer);
+            thread.start();
         } catch (IOException e) {
             e.printStackTrace();
         }
